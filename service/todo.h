861d8718c96c97101aadf325d8742178fe163c3b
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-07-30 18:13:55 +0200

    Update TODO

--- service/todo.h
+++ service/todo.h
@@ -7,10 +7,12 @@
 #define RESTORE "RES"
 #define ISVALID "ISV"
 #define OTHER "OTH"
+#define CODE_LENGTH 17
+#define TODO_SIZE 4
 
 typedef struct toDo {
-    char code[ 17 ];
-    char todo[ 4 ];
+    char code[ CODE_LENGTH ];
+    char todo[ TODO_SIZE ];
 } todo;
 
 #endif // TODO_H
